3c492c5d011cb22e75f60760247874fd96b2ac23
Sujoy <sujoy.n73@gmail.com>
2018-05-31 01:34:00 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+  fs.appendFileSync('log.txt',log + '\n')
+  console.log(log);
+  next();
+});
+
+app.use((req,res) => {
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getYear',() => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('convertToCaps',(text) => {
+    return text.toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+  res.render('Welcome.hbs',{
+    pageTitle: 'Welcome Page',
+    user: 'Sujoy',
+  });
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMessage: 'Unable to find the data!!'
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+  });
+});
+
+app.listen(3000);
